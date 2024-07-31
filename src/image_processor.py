e4c8169409d101d84c5301a419ba8c3d73c9ba26
Shubham Pal <shubham21564@iiitd.ac.in>
2024-07-31 18:34:32 +0530

    Update image_processor.py

--- src/image_processor.py
+++ src/image_processor.py
@@ -1,22 +1,39 @@
+import cv2
+import numpy as np
 import os
-from edge_detection import edge_detection
-from image_conversion import convert_to_bw
 
+def load_image(image_path):
+    return cv2.imread(image_path)
 
-def process_images(input_dir, output_dir):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+def save_image(image, save_path):
+    cv2.imwrite(save_path, image)
 
-    for filename in os.listdir(input_dir):
-        if filename.endswith(".tiff"):
-            input_path = os.path.join(input_dir, filename)
-            output_edge_path = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}_edges.png")
-            output_bw_path = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}_bw.png")
+def edge_detection(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray_image, 100, 200)
+    return edges
 
-            edge_detection(input_path, output_edge_path)
-            convert_to_bw(input_path, output_bw_path)
+def main():
+    input_folder = '../../data/aerials/aerials'
+    output_folder = 'bin/'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-if __name__ == "__main__":
-    input_directory = "../data/aerial"
-    output_directory = "output"
-    process_images(input_directory, output_directory)
+    image_files = []
+    for file in os.listdir(input_folder):
+        if file.endswith('.tiff') or file.endswith('.png'):
+            image_files.append(file)
+    
+
+    for image_file in image_files:
+        image_path = os.path.join(input_folder, image_file)
+        save_path = os.path.join(output_folder, os.path.splitext(image_file)[0] + '.png')
+
+        print(f'Processing {image_path}...')
+        image = load_image(image_path)
+        edges = edge_detection(image)
+        save_image(edges, save_path)
+        print(f'Saved edge-detected image to {save_path}')
+
+if __name__ == '__main__':
+    main()
